5a7a2bd68e2f2c988f2a8c7a3d7408ebc31efaef
Ilya <66596500+loremru@users.noreply.github.com>
2022-02-25 17:46:41 +0300

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,4 @@ import { GraphQLClient } from 'graphql-request'
 
 const client = new GraphQLClient(process.env.HASURA_URL)
 client.setHeader('x-hasura-admin-secret', process.env.HASURA_SECRET)
-client.setHeader('x-hasura-role', 'erp-admin')
-
 export default client
